5f66c01f8d606f9f000dfa4af1fbac0a9a9a58d5
Vanille-N <neven.villani@gmail.com>
2020-10-07 12:51:45 +0200

    clean indentation + todo status + comments

--- game.ml
+++ game.ml
@@ -29,12 +29,14 @@ let game = ref (Rules.new_game [])
 
 (* return the ball that the player wants to move *)
 let rec get_ball game =
+    (* extended to allow user to select either a ball or a key *)
     let status = G.wait_next_event [G.Button_down;G.Key_pressed] in
     if status.G.keypressed = true then begin
         if Char.chr (Char.code status.G.key) = k_quit_game then Abort
         else if Char.chr (Char.code status.G.key) = k_mv_undo then Undo
-        else get_ball game
+        else get_ball game (* not a valid key, keep waiting *)
     end else begin
+        (* check if a ball was selected *)
         let (x,y) = (status.G.mouse_x,status.G.mouse_y) in
         let p = D.position_of_coord x y in
         if Rules.is_ball game p then
@@ -49,140 +51,139 @@ let rec get_ball game =
 
 (* convert the key pressed into a char and call the continuation k on it *)
 let get_key_pressed k =
-  let status = G.wait_next_event [G.Key_pressed] in
-  let key = Char.code status.G.key in
-  k (Char.chr key)
+    let status = G.wait_next_event [G.Key_pressed] in
+    let key = Char.code status.G.key in
+    k (Char.chr key)
 
 (* return the direction choosen by the player *)
 let rec get_ball_direction () =
-  let dir_of_char c =
-    Rules.(
-      match c with
-      | c when c=k_mv_up -> Some Up
-      | c when c=k_mv_dn -> Some Down
-      | c when c=k_mv_rt -> Some Right
-      | c when c=k_mv_lt -> Some Left
-      | c when c=k_mv_abrt -> Some Stay
-      | _ -> None
-    )
-  in
-  get_key_pressed (fun c -> match dir_of_char c with
-      | Some (x) -> x
-      | None -> get_ball_direction () (* wrong key pressed by the player *)
-    )
+    let dir_of_char c =
+        Rules.(
+            List.assoc_opt c [
+                (k_mv_up, Up); (k_mv_dn, Down);
+                (k_mv_rt, Right); (k_mv_lt, Left);
+                (k_mv_abrt, Stay)
+            ]
+        )
+    in
+    get_key_pressed (fun c ->
+        match dir_of_char c with
+            | Some (x) -> x
+            | None -> get_ball_direction () (* wrong key pressed by the player *)
+        )
 
 (* get the next move of the player *)
 let get_next_move game =
     match get_ball game with
         | Ball p -> let d = get_ball_direction () in Move (Rules.make_move p d)
         | Abort -> Abort
-        | Move _ -> failwith "Unreachable @get_next_move::Move"
+        | Move _ -> failwith "Unreachable @get_next_move::Move" (* only get_next_move can create Move *)
         | Undo -> Undo
 
 
 (* create_game allows the player to create its own game by putting balls over the grid *)
 let create_game () =
-  D.ready false;
-  D.draw_game max_x max_y (Rules.new_game []);
-  let ball_count = ref 0 in
-  let rec add_balls l =
-    let status = G.wait_next_event [G.Button_down;G.Key_pressed] in
-    if status.G.keypressed = true && Char.chr (Char.code status.G.key) = k_launch then
-      begin Draw.ready true; l end
-    else
-      let (x,y) = (status.G.mouse_x, status.G.mouse_y) in
-      let p = D.position_of_coord x y in
-      let (x',y') = Position.proj_x p, Position.proj_y p in
-      (* balls can not be outside the grid *)
-      if 0 <= x' && x' < max_x && 0 <= y' && y' < max_y then
-        let ball = Rules.make_ball !ball_count p in
-        incr ball_count;
-        D.draw_ball ball;
-        add_balls (ball::l)
-      else
-        add_balls l
-  in
-  let balls = add_balls [] in
-  Rules.new_game balls
+    D.ready false;
+    D.draw_game max_x max_y (Rules.new_game []);
+    let ball_count = ref 0 in
+    let rec add_balls l =
+        let status = G.wait_next_event [G.Button_down;G.Key_pressed] in
+        if status.G.keypressed = true && Char.chr (Char.code status.G.key) = k_launch then
+            begin Draw.ready true; l end
+        else
+            (* add a ball *)
+            let (x,y) = (status.G.mouse_x, status.G.mouse_y) in
+            let p = D.position_of_coord x y in
+            let (x',y') = Position.proj_x p, Position.proj_y p in
+            (* balls can not be outside the grid *)
+            if 0 <= x' && x' < max_x && 0 <= y' && y' < max_y then
+                (* we don't have to check right now that the position is available because
+                 * game will manage it *)
+                let ball = Rules.make_ball !ball_count p in
+                incr ball_count;
+                D.draw_ball ball;
+                add_balls (ball::l)
+            else
+                add_balls l
+    in
+    let balls = add_balls [] in
+    Rules.new_game balls
 
-(* A menu is a pair of string * f where f is a function of type unit -> unit.
-   If the player choose on the menu which function should be called *)
+(* A menu is a (string * (unit -> unit)) list.
+ * The player chooses which function should be called *)
 let rec menu = [("solve new", solve);("play new", play);("exit", leave)]
 and menu_replay = [("solve new", solve);("resolve", resolve);
                    ("play new", play); ("replay", replay); ("exit", leave)]
 (* [play ()] allows the player to create a new game, and then try to solve it *)
 and play () =
-  game := create_game ();
-  loop (Rules.deep_copy !game)
+    game := create_game ();
+    loop (Rules.deep_copy !game)
 
 (* [solve ()] allows the player to create a new game and then see if the game can be solved *)
 and solve () =
-  game := create_game ();
-  solver (Rules.deep_copy !game)
+    game := create_game ();
+    solver (Rules.deep_copy !game)
 
-(* [loop game] loops on the game while there are still moves possible for the player *)
+(* [loop game] loops on the game until the player chooses to exit
+ * even if there are no possible moves left, allow undoing moves to explore *)
 and loop game =
-  let game = ref game in
-  let allowed = ref (Rules.moves !game) in
-  while !allowed <> [] do
+    let game = ref game in
+    let stay = ref true in (* should we keep looping ? *)
+    while !stay do
+        D.draw_game max_x max_y !game;
+        let user = get_next_move !game in
+        match user with
+            | Move user ->
+                if List.mem user (Rules.moves !game) then
+                    (* { b; Stay } is never allowed regardless of b,
+                    * ensuring that Stay will never be converted into a Position.t *)
+                    game := Rules.apply_move !game user
+            | Abort -> stay := false
+            | Ball _ -> failwith "Unreachable @loop::Ball" (* get_next_move will convert Ball -> Move *)
+            | Undo -> game := Rules.undo_move !game
+    done;
     D.draw_game max_x max_y !game;
-    let user = get_next_move !game in
-    match user with
-        | Move user ->
-            if List.mem user !allowed then begin
-                (* { b; Stay } is never allowed regardless of b,
-                 * ensuring that Stay will never be converted into a Position.t *)
-                game := Rules.apply_move !game user;
-                allowed := Rules.moves !game;
-            end
-        | Abort -> allowed := []
-        | Ball _ -> failwith "Unreachable @loop::Ball"
-        | Undo -> (
-            game := Rules.undo_move !game;
-            allowed := Rules.moves !game;
-            )
-  done;
-  D.draw_game max_x max_y !game;
-  get_key_pressed (fun c -> ());
-  main menu_replay
+    main menu_replay
 
 (* [solver game] solves the game if it is possible *)
 and solver game  =
-  D.draw_game max_x max_y game;
-  let moves = Solver.solve game in
-  match moves with
-  | None -> D.draw_string "No solution!"; get_key_pressed (fun c -> main menu)
-  | Some moves ->
-    let g = List.fold_left (fun g m -> D.draw_game max_x max_y g ;
-                             D.draw_string "Solved!";
-                             get_key_pressed (fun c -> ());
-                             Rules.apply_move g m) game moves
-    in
-    D.draw_game max_x max_y g;
-    get_key_pressed (fun c -> main (("resolve", resolve)::menu))
+    D.draw_game max_x max_y game;
+    let moves = Solver.solve game in
+    match moves with
+        | None -> D.draw_string "No solution!"; get_key_pressed (fun c -> main menu)
+        | Some moves ->
+            let g = List.fold_left (fun g m ->
+                D.draw_game max_x max_y g ;
+                D.draw_string "Solved!";
+                get_key_pressed (fun c -> ());
+                Rules.apply_move g m
+            ) game moves
+            in
+            D.draw_game max_x max_y g;
+            get_key_pressed (fun c -> main (("resolve", resolve)::menu))
 
 (* replay the previous game *)
 and replay () =
-  loop (Rules.deep_copy !game)
+    loop (Rules.deep_copy !game)
 (* resolve the previous game *)
 and resolve () =
-  solver (Rules.deep_copy !game)
+    solver (Rules.deep_copy !game)
 (* leave the application *)
 and leave () =
-  D.close_window()
+    D.close_window()
 
 (* get the choice of the player *)
 and main l =
-  let choice c =
-    let i = (int_of_char c) - (int_of_char '0') in
-    if 0 <= i && i < List.length l then
-      snd (List.nth l i) ()
-    else
-      main l
-  in
-  Random.self_init();
-  D.init_window();
-  D.draw_menu l;
-  get_key_pressed choice
+    let choice c =
+        let i = (int_of_char c) - (int_of_char '0') in
+        if 0 <= i && i < List.length l then
+            snd (List.nth l i) ()
+        else
+            main l
+    in
+    Random.self_init ();
+    D.init_window ();
+    D.draw_menu l;
+    get_key_pressed choice
 
 let _ = main menu
--- rules.ml
+++ rules.ml
@@ -7,6 +7,7 @@ let pos_of_dir = function
     | Right -> Position.from_int 1 0
     | Stay -> failwith "Unreachable @pos_of_dir::Stay"
 
+(** a single operation of changing a ball's position *)
 type displacement = {
     id: int; (* ball that was displaced *)
     old_pos: Position.t;
@@ -36,6 +37,12 @@ let hist_push g id old_pos new_pos =
     g.hist <- { id=id; old_pos=old_pos; new_pos=new_pos; } :: g.hist
 
 let hist_pop g =
+    (* get displacements until either:
+     * - hist is empty (gone back to initial state)
+     * - next element has no new_pos
+     * because a sequence of displacements *has* to end with a ball that went off the grid
+     * => no new_pos means last element of previous move
+     *)
     let rec aux b disps hist =
         match hist with
             | [] -> (disps, [])
@@ -59,11 +66,15 @@ let ball_of_position game p =
 
 let new_game bs =
     let balls = Hashtbl.create (grid_width + grid_height) in
+    let grid = Hashtbl.create (grid_width + grid_height) in
     (* grid.width is a first guess at how many balls there will be.
      * ~one per column/line is a reasonable ballpark *)
-    List.iter (fun b -> Hashtbl.add balls b.id b.pos) bs;
-    let grid = Hashtbl.create (grid_width + grid_height) in
-    List.iter (fun b -> Hashtbl.add grid b.pos b.id) bs;
+    List.iter (fun b ->
+        if not (Hashtbl.mem grid b.pos) then begin
+            Hashtbl.add balls b.id b.pos;
+            Hashtbl.add grid b.pos b.id
+            end
+            ) bs;
     { balls=balls; grid=grid; hist=[]; }
 
 let eq_ball b b' =
@@ -73,6 +84,7 @@ let make_move b d =
     { ball=b; dir=d; }
 
 let is_ball g p =
+    (* we just have to see if p exists in g.grid *)
     Hashtbl.mem g.grid p
 
 let is_inside p =
@@ -80,40 +92,51 @@ let is_inside p =
     and y = Position.proj_y p in
     0 <= x && x < grid_width && 0 <= y && y <= grid_height
 
+(* each iteration of [apply_move] calculates the new position for a single ball
+ * and recursively propagates the move to the 0 or 1 balls that were hit.
+ * It stops when a ball goes off the edge.
+ *)
 let rec apply_move g move =
     let p' = pos_of_dir move.dir in
     let p = ref move.ball.pos in
-    while (is_inside !p) && not (is_ball g (Position.move !p p')) do
-        p := Position.move !p p';
+    let pnext = ref (Position.move !p p') in
+    (* find another ball or the edge *)
+    while (is_inside !p) && not (is_ball g !pnext) do
+        p := !pnext;
+        pnext := Position.move !p p';
     done;
     if is_inside !p then begin
         (* hit another ball, we have a few modifications to make *)
         let id_move = move.ball.id in
+        (* update accessors *)
         Hashtbl.replace g.balls id_move !p;
         Hashtbl.remove g.grid move.ball.pos;
         Hashtbl.add g.grid !p id_move;
         (* add move to the history of g *)
         hist_push g id_move move.ball.pos (Some !p);
-        (* and we have to propagate the move to the ball we hit *)
-        apply_move g (make_move (ball_of_position g (Position.move !p p')) move.dir)
+        (* propagate the move to the ball we hit *)
+        apply_move g (make_move (ball_of_position g !pnext) move.dir)
     end else begin
         (* did not hit a ball, ball goes off the edge *)
         let id_remove = Hashtbl.find g.grid move.ball.pos in
         Hashtbl.remove g.grid move.ball.pos;
         Hashtbl.remove g.balls id_remove;
+        (* add move to the history of g
+         * this marks the end of a move (see more in pop_hist) *)
         hist_push g id_remove move.ball.pos None;
         g
     end
 
 let undo_move g =
     let disps = hist_pop g in
+    (* for each displacement... *)
     List.iter (fun disp ->
         match disp.new_pos with
-            | Some p ->
+            | Some p -> (* ball stayed inside the grid, move it *)
                 Hashtbl.remove g.grid p;
                 Hashtbl.add g.grid disp.old_pos disp.id;
                 Hashtbl.replace g.balls disp.id disp.old_pos
-            | None ->
+            | None -> (* ball went off the edge, put it back in *)
                 Hashtbl.add g.grid disp.old_pos disp.id;
                 Hashtbl.add g.balls disp.id disp.old_pos
         ) disps;
@@ -121,25 +144,31 @@ let undo_move g =
 
 let moves g =
     let mv = ref [] in
+    (* for one ball first, then we'll iterate over all balls *)
     let find_moves_ball id =
+        (* for each possible move: *)
         List.iter (fun m ->
                 let pos = Hashtbl.find g.balls id in
                 let p' = pos_of_dir m in
                 let p = ref (Position.move pos p') in
+                (* find another ball or the edge *)
                 while (is_inside !p) && not (is_ball g !p) do
                     p := Position.move !p p';
                 done;
+                (* it was a ball, the move is valid *)
                 if is_ball g !p then
                     mv := (make_move (make_ball id pos) m) :: !mv;
             ) [Up; Down; Right; Left]
     in
     g.balls
+    (* equivalent to Hashtbl.keys, yields all balls still in game *)
     |> fun h -> Hashtbl.fold (fun k v acc -> k :: acc) h []
     |> List.iter find_moves_ball;
     !mv
 
 let get_balls g =
     g.balls
+    (* equivalent to Hashtbl.items *)
     |> fun h -> Hashtbl.fold (fun k v acc -> (k, v) :: acc) h []
     |> List.map (fun (i, p) -> make_ball i p)
 
