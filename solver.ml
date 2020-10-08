dcb2cc2da3077974072380d8fd97efe90b729abb
Vanille-N <neven.villani@gmail.com>
2020-10-08 16:54:21 +0200

    implemented solver

--- solver.ml
+++ solver.ml
@@ -1,1 +1,53 @@
-let solve game = failwith "TODO solve"
+type async_solver = {
+    mutable game: Rules.game;
+    mutable fork: Rules.move list list;
+    mutable found: bool;
+    mutable count: int;
+}
+
+let solve game = {
+    game=game;
+    fork=[Rules.moves game];
+    found=false;
+    count=0;
+}
+
+let game solver = solver.game
+
+let count solver = solver.count
+
+let is_solved solver = solver.found
+
+let step solver =
+    solver.count <- solver.count + 1;
+    if solver.found then (
+        match solver.fork with
+            (* we went back to the base level *)
+            | [] -> Some true
+            (* we have some moves to undo *)
+            | hd::tl ->
+                solver.game <- Rules.undo_move solver.game;
+                solver.fork <- tl;
+                None
+    ) else (
+        match solver.fork with
+            (* there are no possible paths left and no solution was found *)
+            | [] -> Some false
+            | []::tl -> (
+                (* there are no possible moves left for this configuration *)
+                if Rules.is_win solver.game then (
+                    solver.found <- true;
+                    None
+                ) else (
+                    solver.game <- Rules.undo_move solver.game;
+                    solver.fork <- tl;
+                    None
+                )
+            )
+            | (m::more)::tl -> (
+                (* m is a possible unexplored move *)
+                solver.game <- Rules.apply_move solver.game m;
+                solver.fork <- (Rules.moves solver.game)::more::tl;
+                None
+                )
+    )
--- solver.mli
+++ solver.mli
@@ -1,1 +1,18 @@
-val solve : Rules.game -> Rules.move list option
+type async_solver
+
+(** [solve] doesn't really solve the problem.
+  * instead, it returns an object that knows how to solve it *)
+val solve : Rules.game -> async_solver
+
+(** each call to [step] advances the solver's computation.
+  *     | None -> not done
+  *     | Some(true) -> solved
+  *     | Some(false) -> no solution
+  *)
+val step : async_solver -> bool option
+
+(** get the current state of the game *)
+val game : async_solver -> Rules.game
+
+(** get the number of steps made by the solver *)
+val count : async_solver -> int
