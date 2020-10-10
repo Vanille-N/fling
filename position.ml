344d98b9708ad84f3b1b0f0ce037ceb80f000ff4
Vanille-N <neven.villani@gmail.com>
2020-10-10 10:32:32 +0200

    fmt

--- position.ml
+++ position.ml
@@ -1,6 +1,6 @@
 type t = int * int
 
-let from_int x y = (x,y)
+let from_int x y = (x, y)
 
 let proj_x = fst
 
@@ -8,6 +8,6 @@ let proj_y = snd
 
 let eq x y = x = y
 
-let move (x,y) (x',y') = (x+x',y+y')
+let move (x, y) (x', y') = (x + x',y + y')
 
-let to_string (x,y) = Printf.sprintf "(%s,%s)" (string_of_int x) (string_of_int y)
+let to_string (x, y) = Printf.sprintf "(%s, %s)" (string_of_int x) (string_of_int y)
