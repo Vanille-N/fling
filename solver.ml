0c0e5e3ed9f6dc02d165cb7621019184fe364b34
Vanille-N <neven.villani@gmail.com>
2020-10-10 10:32:10 +0200

    attempt at a heuristic

--- solver.ml
+++ solver.ml
@@ -18,6 +18,37 @@ let count solver = solver.count
 
 let is_solved solver = solver.found
 
+(* let has_extremal g =
+    (* criterion: if a ball has an extremum for both its coordinates then there is no solution *)
+    let (minx, miny, maxx, maxy) = (ref 100, ref 100, ref (-1), ref (-1)) in
+    let (cmnx, cmny, cmxx, cmxy) = (ref 0, ref 0, ref 0, ref 0) in
+    List.iter (fun b ->
+        let p = Rules.position_of_ball b in
+        let x = Position.proj_x p in
+        let y = Position.proj_y p in
+        if x = !minx then incr cmnx;
+        if x = !maxx then incr cmxx;
+        if x < !minx then (minx := x; cmnx := 0);
+        if x > !maxx then (maxx := x; cmxx := 0);
+        if y = !miny then incr cmny;
+        if y = !maxy then incr cmxy;
+        if y < !miny then (miny := y; cmny := 0);
+        if y > !maxy then (maxy := y; cmxy := 0);
+        ) (Rules.get_balls g);
+    let rec aux = function
+        | [] -> true
+        | b::tl -> (
+            let p = Rules.position_of_ball b in
+            let x = Position.proj_x p in
+            let y = Position.proj_y p in
+            if ((x = !maxx && !cmxx = 1) || (x = !minx && !cmnx = 1))
+            && ((y = !maxy && !cmxy = 1) || (y = !miny && !cmny = 1))
+            then false
+            else aux tl
+        )
+    in aux (Rules.get_balls g) *)
+
+
 let step solver =
     solver.count <- solver.count + 1;
     if solver.found then (
@@ -49,8 +80,9 @@ let step solver =
             )
             | (m::more)::tl -> (
                 (* m is a possible unexplored move *)
-                solver.game <- Rules.apply_move solver.game m;
+                let g = Rules.apply_move solver.game m in
+                solver.game <- g;
                 solver.fork <- (Rules.moves solver.game)::more::tl;
                 None
-                )
+            )
     )
