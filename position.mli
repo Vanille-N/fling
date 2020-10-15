9f98c157f1ac312f2bd646eded11487f33e1f78b
Vanille-N <neven.villani@gmail.com>
2020-10-15 08:39:29 +0200

    doc position

--- position.mli
+++ position.mli
@@ -2,14 +2,20 @@
 
 type t
 
+(** Initialize a new position *)
 val of_ints : int -> int -> t
 
+(** Extract first coordinate *)
 val proj_x : t -> int
 
+(** Extract second coordinate *)
 val proj_y : t -> int
 
+(** Check if two positions are the same *)
 val eq : t -> t -> bool
 
+(** Vector addition on positions *)
 val move : t -> t -> t
 
+(** Pretty-print coordinates *)
 val to_string : t -> string
